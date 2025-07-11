190d7235eaccfea7e78ff19d78fc5a6e148c0c44
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-11 20:31:19 -0300

    Atualização

--- Aprendizado/Curso/Introdução/PEP8.py
+++ Aprendizado/Curso/Introdução/PEP8.py
@@ -10,6 +10,7 @@ A primeira forma eh a correta , inicio maiusculo e sem o underline, POO
 class CalculadoraCientifica:
     pass
 
+
 class calculadora_cientifica:
     pass
 
@@ -23,7 +24,47 @@ def soma():
 def soma_dois():
     pass
 
-numero = 4;
+numero = 4
 numero_impar = 3
 
+"""
+Utilize 4 espacos para identação
+"""
+
+if 'e' in 'banana':
+    print('tem')
+else:
+    print('nao tem')
+
+"""
+Linhas em brancos
+-Seperar funções e definições de duas linhas em branco
+-Metodos dentro de uma classe deve ser separado por apenas uma linha em branco
+"""
+
+class Classe:
+    pass
+
+class Outra:
+    pass
+
+#Import Errado
+#import sys, os
+
+#Import Certo
+#import sys
+#import os
+
+"""
+Imports devem ser colocados no topo do arquivo
+"""
+
+"""
+Nunca dar espaço entre valores de função
+"""
+#Errado:
+#funcao( algo[ 1 ], {outro: 2} )
+
+#Certo:
+#funcao(algo[1], {outro: 2})
 
