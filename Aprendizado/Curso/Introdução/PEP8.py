5bdd6bcb7dd268ebc711c493d2d124cedb791823
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-11 20:14:39 -0300

    Atualização

--- Aprendizado/Curso/Introdução/PEP8.py
+++ Aprendizado/Curso/Introdução/PEP8.py
@@ -1,7 +1,29 @@
 """
 Propostas de melhoria para a linguagem phyton
 
-A ideia da PEP8 eh que possamos escrever codigos phyton de forma phytonica
+A ideia da PEP8 eh que possamos escrever codigos phyton de forma phytonica(forma bonita)
 """
 
-print("Hello world")
+"""
+A primeira forma eh a correta , inicio maiusculo e sem o underline, POO
+"""
+class CalculadoraCientifica:
+    pass
+
+class calculadora_cientifica:
+    pass
+
+"""
+Utilize nomes em minusuculos separados por underline para funcoes ou variaveis, todos metodos as baixos estão certos
+"""
+
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+numero = 4;
+numero_impar = 3
+
+
