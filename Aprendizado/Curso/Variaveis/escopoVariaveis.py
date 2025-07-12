ad6398384d37e0aaf7fcd1caa8b137cee80187f9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-12 14:42:51 -0300

    Atualização

---
+++ Aprendizado/Curso/Variaveis/escopoVariaveis.py
@@ -0,0 +1,17 @@
+numero = 42
+print(numero)
+print(type(numero))
+
+numero = "Reatribuicao"
+print(numero)
+print(type(numero))
+# Em py eh possivel fazer a reatribuição
+
+numero = 42
+
+if numero > 10:
+    novo = numero - 10
+    print(novo)
+else:
+    novo = numero + 10
+    print(novo)
