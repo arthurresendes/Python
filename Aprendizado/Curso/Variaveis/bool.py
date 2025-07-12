1631b8720aefb34565e3eebf7cb403e2d05ae6d3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-12 13:46:58 -0300

    Atualização

---
+++ Aprendizado/Curso/Variaveis/bool.py
@@ -0,0 +1,14 @@
+"""
+Sempre o True e Flase iniciar com letra maiuscula
+
+"""
+user = "Arthur"
+ativo = True
+
+print(user)
+#ativo = True
+if user == ativo:
+    print(ativo)
+else:
+    print(not ativo)
+
