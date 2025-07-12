2f756cfef43f7669912b96f6739f4cd57b45705d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-12 13:14:12 -0300

    Atualização

---
+++ Aprendizado/Curso/Variaveis/numerico.py
@@ -0,0 +1,20 @@
+num = 42 
+print(num)
+num2 = 50
+print(num2)
+
+soma = num + num2
+print(soma)
+
+divisao = soma / 2
+print(divisao)
+
+soma = soma + 1
+resto = soma % 2
+print(resto)
+
+elevado = num ** 2
+print(elevado)
+
+# Ctrl + l -> limpa o console
+# type(nome variavel) -> retorna qual o tipo de dado trabalhado
