606a1fcb146f1443528e62c7596ef54cecc60ffe
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-11 21:04:25 -0300

    Atualização

---
+++ Aprendizado/Curso/Introdução/dirAndHelp.py
@@ -0,0 +1,9 @@
+"""
+
+Dir- Apresenta todos os atributos e funções/metodos disponiveis para determinado tipo de dado ou variavel, usa ""
+dir(tipo de dado/variavel)
+
+Help-Apresenta  a documentação e como utilizar os atributos/propriedades e funções//metodos disponiveis para determinado tipo de dado ou variavel, usa ''
+help(tipo de dado/variavel.propriedade)
+
+"""
