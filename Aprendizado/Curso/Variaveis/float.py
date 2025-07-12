56deaf3a85493b518cbd2b0707c1010bb5a5246c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-12 13:30:59 -0300

    Atualização

---
+++ Aprendizado/Curso/Variaveis/float.py
@@ -0,0 +1,30 @@
+"""
+Casas decimais são seperadas por pontos , pois a separação no inglês eh feita em ponto
+"""
+
+valor = 1.44
+print(valor)
+print(type(valor))
+
+
+num1 , num2 = 1, 2
+print(num1)
+print(type(num1))
+print(num2)
+print(type(num2))
+
+
+# Podemos converter float para int
+res = int(valor)
+print(res)
+print(type(res))
+
+# E tambem vice-versa
+converte = float(num1)
+print(converte)
+print(type(converte))
+
+# Podemos trabalhar com números complexos (j) ex: 1j
+complexo = 5j
+print(complexo)
+print(type(complexo))
