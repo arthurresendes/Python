93531b6539cab348f026787f066849beb148d7ae
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-14 19:44:11 -0300

    Atualização

---
+++ Aprendizado/Curso/Condição/cond001.py
@@ -0,0 +1,25 @@
+"""
+
+Condicionais:
+
+if- se
+
+elif = else if - senão se
+
+else - senao
+
+"""
+
+idade = int(input("Digite sua idade: "))
+
+if idade < 18:
+
+    print("Menor de idade!!")
+
+elif idade == 18:
+
+    print("Você tem 18 ou 19 anos!!")
+
+else:
+
+    print("Você tem mais de 18 anos!!")
