80b55c9af9b8072feb4fcc1981d4e29d439374f6
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-14 19:45:21 -0300

    Atualização

---
+++ Aprendizado/Curso/Condição/cond002.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite numero 1: "))
+
+num2 = int(input("Digite numero 2: "))
+
+if num1 > num2:
+
+    print("Numero 1 eh maior que numero 2")
+
+else:
+
+    print("Numero 2 eh maior que numero 1")
