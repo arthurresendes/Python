c759bab95079fd5adaa9f4493b18180fa1543a66
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-13 21:06:22 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/py008.py
@@ -0,0 +1,5 @@
+import math
+
+num = float(input("Digite um numero: "))
+rzQuadrada = math.sqrt(num)
+print(f"Seu número: {num}\nDobro do número: {num * 2}\nTriplo do número: {num * 3}\nRaiz Quadrada do número: {rzQuadrada}")
