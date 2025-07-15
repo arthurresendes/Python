5fc88734022a3253eb8fd83ef6a30b4a2665f9d5
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-15 20:33:09 -0300

    Atualização

---
+++ Aprendizado/Curso/loops/range001.py
@@ -0,0 +1,27 @@
+#For de 0 a 10
+for i in range(11):
+    print(i)
+    
+print('\n')
+
+#For de 1 ate 10
+for j in range(1,11):
+    print(j)
+
+print('\n')
+
+#for comeca no 1 vai ate 10 pulando de dois em dois
+for x in range(1,11,2):
+    print(x)
+    
+print('\n')
+
+#Loop ao contrario
+for y in range(10 , 0 ,-1):
+    print(y)
+    
+"""
+Para fazer no terminal
+lista = list(range(10))
+
+"""
