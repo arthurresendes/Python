f2d59bf81f08fba1282682d481ee195efa24aa6e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-16 17:29:07 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/py013.py
@@ -0,0 +1,12 @@
+num = 0
+while num <= 0:
+
+    num = int(input("Digite um numero : "))
+
+comecaTabuada = 1
+
+while comecaTabuada <= 10:
+
+    print(f"{num} * {comecaTabuada} = {num * comecaTabuada}")
+
+    comecaTabuada +=1
