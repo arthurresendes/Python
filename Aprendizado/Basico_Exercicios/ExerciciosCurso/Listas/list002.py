fdcb30c372022475194b04141966b657a3ec0a10
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-16 17:34:56 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosCurso/Listas/list002.py
@@ -0,0 +1,13 @@
+lista = [1, 0, 5, -2, -5, 7]
+
+soma = lista[0] + lista[1] + lista[5]
+
+print(f"A soma dos elementos 0, 1 e 5 é: {soma}")
+
+lista[5] = 100
+
+print(f"Novo valor do elemento na posição [5]: {lista[5]}")
+
+for i in lista:
+
+    print(i)
