30c1dba301396d7b265faf9b9a203df7646e42d4
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 19:48:55 -0300

    Atualização

---
+++ Aprendizado/Curso/Coleções/dicionarios.py
@@ -0,0 +1,16 @@
+"""
+
+Dicionarios são coleções do tipo chave/valor
+
+Obs: Em algumas linguagens de programação são chamados de mapas ou tabelas hash
+
+Dicionario são representados por {} , chaves e valor são separados por dois pontos (:) e os itens por virgula (,), podendo ser qualquer tipo de dados , podendo ter uma mescla de tipo de dados
+
+
+"""
+
+print(type({}))
+
+print("Os dicionario são feitos por chave e seu devido valor. Segue exemplo: ")
+paises = {'br': 'Brasil' , 'eua': 'Estados Unidos' , 'fr': 'França'}
+print(paises)
