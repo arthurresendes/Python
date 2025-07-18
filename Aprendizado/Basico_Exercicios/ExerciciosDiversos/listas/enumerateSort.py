b4a12fb1168a475c1ad0844135166f30a74c6383
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 14:14:43 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/enumerateSort.py
@@ -0,0 +1,5 @@
+lista = [2,4,1,7,4,8,9,1,12,4,17,22,45,2,31]
+
+lista.sort()
+for indice , num in enumerate(lista):
+    print(f"Indice: {indice} , valor: {num}")
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/menorList.py
@@ -0,0 +1,6 @@
+lista = [2,4,1,7,4,8,9,1,12,4,17,22,45,2,31]
+
+menor = min(lista)
+
+quantidade = lista.count(menor)
+print(f"O menor numero eh {menor} e aperece {quantidade} vezes")
