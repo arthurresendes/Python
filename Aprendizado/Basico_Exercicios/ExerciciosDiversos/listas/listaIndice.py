5c16c0cbb7e1f66f98875dee6ce38faff72331b6
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 14:05:36 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/listaIndice.py
@@ -0,0 +1,7 @@
+lista = ["maçã", "banana", "uva", "laranja", "pera"]
+
+indice = 0
+
+while indice < len(lista):
+    print(lista[indice])
+    indice += 1
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/soma.py
@@ -0,0 +1,8 @@
+lista = [1, 2, 3, 4, 5]
+
+soma = 0
+
+for i in range(len(lista)):
+    soma = soma + i
+
+print(soma)
