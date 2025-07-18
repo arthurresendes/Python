c7c48eef5d2408e9563a2fc673d22a4a54c2b334
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 13:55:10 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/forLista.py
@@ -0,0 +1,6 @@
+lista = []
+
+for valor in range(1 ,11):
+    lista.append(valor)
+
+print(lista)
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/forNameList.py
@@ -0,0 +1,7 @@
+lista = []
+
+for i in range(3):
+    nome = input(f"Digite o nome {i+1}: ")
+    lista.append(nome)
+
+print(lista)
