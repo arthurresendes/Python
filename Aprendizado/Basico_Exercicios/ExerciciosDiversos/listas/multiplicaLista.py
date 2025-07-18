22e35805063111be76bfb6cbcbbc35728b420be7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 14:03:14 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/multiplicaLista.py
@@ -0,0 +1,9 @@
+lista = [1, 2, 3, 4, 5]
+
+nova_lista = []
+
+for i in lista:
+    print(i * 2)
+    nova_lista.append(i * 2)
+
+print(nova_lista)
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/listas/ordenaReverte.py
@@ -0,0 +1,6 @@
+lista = [4, 1, 7, 3, 2]
+
+lista.sort()
+print(lista)
+lista.reverse()
+print(lista)
