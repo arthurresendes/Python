ceb63059dc82df0a9e68d745ecb31eaef699a973
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-18 21:13:50 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Dicionarios/pessoa.py
@@ -0,0 +1,6 @@
+pessoa = {
+    'nome': input("Digite seu nome: "),
+    'idade': int(input("Digite sua idade: ")),
+    'altura': input("Digite sua altura: ") 
+}
+print(pessoa)
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Dicionarios/tresPaises.py
@@ -0,0 +1,7 @@
+dicionario = {
+    'Brasil': 'Brasilia',
+    'Estados Unidos': 'Washignton',
+    'Russia': 'Moscou'
+}
+
+print(dicionario)
