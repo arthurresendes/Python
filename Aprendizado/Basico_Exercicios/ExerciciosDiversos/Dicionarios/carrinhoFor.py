60d274249f8e7d0349d4ee3e423650b66293a8e6
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-19 13:25:17 -0300

    Atualização

--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/Dicionarios/carrinhoFor.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Dicionarios/carrinhoFor.py
@@ -3,3 +3,11 @@ carrinho = [
     {'nome': 'Boné', 'preco': 30.0, 'quantidade': 1},
     {'nome': 'Tênis', 'preco': 120.0, 'quantidade': 1}
 ]
+
+total = 0
+
+for i in carrinho:
+    subTotal = i['preco'] * i['quantidade']
+    total += subTotal
+    
+print(f"Total do carrinho: R$ {total:.2f}")
