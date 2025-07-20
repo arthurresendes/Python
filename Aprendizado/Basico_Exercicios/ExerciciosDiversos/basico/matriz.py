1815a2295e37baa23a4ce3f63777f86f4543709e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-20 14:29:36 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/basico/matriz.py
@@ -0,0 +1,8 @@
+matriz = [[0,0,0], [0,0,0] , [0,0,0]]
+
+for i in range(3):
+    for j  in range(3):
+        matriz[i][j] = int(input("Digite um numero: "))
+
+for linha in matriz:
+    print(linha)
