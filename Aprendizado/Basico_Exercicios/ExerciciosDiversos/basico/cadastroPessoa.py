298707f93781b5701d2f8d0e7d25fc0af1298200
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-20 14:15:33 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/basico/cadastroPessoa.py
@@ -0,0 +1,20 @@
+pessoa1 = {
+    'nome': input("Digite seu nome: "),
+    'idade': int(input("Digite sua idade: ")),
+    'cidade': input("Digite sua cidade: ")
+}
+
+pessoa2 = {
+    'nome': input("Digite seu nome: "),
+    'idade': int(input("Digite sua idade: ")),
+    'cidade': input("Digite sua cidade: ")
+}
+
+pessoa3 = {
+    'nome': input("Digite seu nome: "),
+    'idade': int(input("Digite sua idade: ")),
+    'cidade': input("Digite sua cidade: ")
+}
+
+lista = [pessoa1, pessoa2, pessoa3]
+print(lista)
