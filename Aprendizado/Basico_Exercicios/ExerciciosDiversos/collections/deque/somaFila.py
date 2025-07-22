20a95c2ec4e3d34221b4e183e10a745ace37219c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-22 15:20:34 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/collections/deque/somaFila.py
@@ -0,0 +1,11 @@
+from collections import deque as dq
+
+fila = dq()
+
+fila = [1,2,3,4,5,6,7,8,9]
+print(fila)
+media = (fila[6] +  fila[7] +  fila[8])/3
+print("Ultimos 3 numeros")
+print(fila[6],fila[7] ,fila[8])
+print("Media: ")
+print(media)
