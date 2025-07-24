124ba63837bbcba25f5e554873362661695d1a29
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-24 18:32:56 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Funcoes/argumentosNomeados.py
@@ -0,0 +1,4 @@
+def dados_pessoais(nome,idade,cidade):
+    return f"Ola, {nome} você tem {idade} anos e mora em {cidade}"
+
+print(dados_pessoais(cidade = "Osasco" , nome = "Arthur" , idade = "18"))
