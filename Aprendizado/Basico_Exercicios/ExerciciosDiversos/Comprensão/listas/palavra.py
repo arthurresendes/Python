b1dff048e6cf7992bccca8e74e3291fa7c2f5b01
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-25 20:52:17 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listas/palavra.py
@@ -0,0 +1,2 @@
+lista = ["arthur", "pedro", "isac"]
+print([nome for nome in lista if nome[0] in 'aeiou'])
--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listas/ultimaMaiuscula.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listas/ultimaMaiuscula.py
@@ -1,5 +1,5 @@
 nomes = ["arthur", "pedro", "isac"]
-
+# nome[:-1] pega todas letras menos a ultima , ja o nome[-1] pega a ultima e deixa maiuscula
 resultado = [nome[:-1] + nome[-1].upper() for nome in nomes]
 
 print(resultado)
