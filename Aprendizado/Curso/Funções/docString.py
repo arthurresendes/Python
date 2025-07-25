c64428eec746695ac6107fc1d35b02cf8d97cb07
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-25 19:22:48 -0300

    Atualização

---
+++ Aprendizado/Curso/Funções/docString.py
@@ -0,0 +1,13 @@
+"""
+Docstrings
+Serve para documentar funções
+
+"""
+
+def diz_oi():
+    """Uma função simples que retorna 'Oi!'"""
+    return 'Oi!'
+
+print(diz_oi())
+
+# Para ver doc no terminal use var.__doc__
