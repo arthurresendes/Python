5153338f6a5af837f7f94a2ccef5726b07856be3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-27 14:11:21 -0300

    Atualização

--- Aprendizado/Curso/Comprehensios/dictionary.py
+++ Aprendizado/Curso/Comprehensios/dictionary.py
@@ -13,3 +13,17 @@ num = {'a': 1, 'b': 2, 'c': 3, 'd':4, 'e':5}
 quadrado = {chave: valor ** 2 for chave, valor in num.items()}
 
 print(quadrado)
+
+numeros = [1,2,3,4,5]
+valorQuadrado = {valor: valor ** 2 for valor in numeros}
+print(valorQuadrado)
+
+chaves =  'abcde'
+valores = [1,2,3,4,5]
+mistura = {chaves[i]: valores[i] for i in range(0, len(chaves))}
+print(mistura)
+
+nums = [1,2,3,4,5]
+
+res = {num:('par' if num % 2 == 0 else 'impar') for num in nums}
+print(res)
