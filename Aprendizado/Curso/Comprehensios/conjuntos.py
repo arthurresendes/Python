91e892377ad35cb479f303589532dca48d56322c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-27 14:24:39 -0300

    Atualização

--- Aprendizado/Curso/Comprehensios/conjuntos.py
+++ Aprendizado/Curso/Comprehensios/conjuntos.py
@@ -4,3 +4,13 @@ Compreensão de conjuntos
 
 numeros = {num for num in range(1,7)}
 print(numeros)
+
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+# Dicionario
+numeros = {x: x ** 2 for x in range(10)}
+print(numeros)
+
+letras = {letra for letra in 'Arthur Resende'.upper()}
+print(letras)
+
