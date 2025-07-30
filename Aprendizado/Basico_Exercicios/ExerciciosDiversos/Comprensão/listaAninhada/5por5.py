8b1eb93a38aac788938c76cb7f708ec08242b070
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-30 19:04:46 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listaAninhada/5por5.py
@@ -0,0 +1,11 @@
+matriz = [[1 if i == j else 0 for j in range(5)]for i in range(5)]
+print(matriz)
+
+"""
+for i in range(5):
+for j in range(5):
+if i == j :
+    1
+else :
+0
+"""
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listaAninhada/inverteMatriz.py
@@ -0,0 +1,2 @@
+original = [[1,2,3], [4,5,6], [7,8,9]]
+invertida = [linha[::-1] for linha in original]
