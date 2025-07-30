81948ef7e8480f95d03c26155f7be347c91e97e2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-07-30 18:55:28 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listaAninhada/matrizIdentidade.py
@@ -0,0 +1,2 @@
+identidade = [[1 if i == j else 0 for j in range(3)] for i in range(3)]
+print(identidade)
--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listaAninhada/rangeMatriz.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Comprensão/listaAninhada/rangeMatriz.py
@@ -1,2 +1,11 @@
 matriz = [[col + (linha * 3) for col in range(1,4)] for linha in range(3)]
 print(matriz)
+
+"""
+
+matriz = []
+for linha in range(3):
+    for col in range(1,4):
+        col = col + (linha * 3)
+        matriz.append(col)
+"""
