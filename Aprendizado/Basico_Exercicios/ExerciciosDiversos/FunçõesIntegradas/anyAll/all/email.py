8b40b0b49366cb9b03f662a32fef58968ebaf366
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-01 14:55:34 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/anyAll/all/email.py
@@ -0,0 +1,4 @@
+
+emails = ["user@gmail.com", "admin@empresa.com", "testeem@presa.com"]
+
+print(all('@' in em for em in emails))
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/anyAll/all/negativo.py
@@ -0,0 +1,3 @@
+valores = [10, 20, 5, -1]
+
+print(all(n > 0 for n in valores))
