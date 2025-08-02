11e00fa9f5436fd295e7a0e78a32673105e7de56
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-02 15:12:30 -0300

    Atualização

--- Aprendizado/Curso/Funções Integradas/minMax.py
+++ Aprendizado/Curso/Funções Integradas/minMax.py
@@ -2,4 +2,19 @@
 min- Minimo
 max - Maximo
 
+
+key -> Recebe uma função
 """
+
+musicas = [
+    {'titulo': 'Manuel Gomes', 'tocou': 3},
+    {'titulo': 'Zeca', 'tocou': 4},
+    {'titulo': 'Thiaguinho', 'tocou': 5},
+    {'titulo': 'Ski', 'tocou': 2}
+]
+
+print(min(musicas, key=lambda x: x['tocou']))
+print(max(musicas, key=lambda x: x['tocou']))
+
+print(min(musicas, key=lambda x: x['tocou'])['titulo'])
+print(max(musicas, key=lambda x: x['tocou'])['titulo'])
