04fa68743c676c3fcf0080aef754ee76366fee9a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-02 14:26:36 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/dict.py
@@ -0,0 +1,2 @@
+dados = {"ana": 9, "bruno": 7, "carlos": 10}
+# Transforme em lista e ordene os itens pelos valores.
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/tuplas.py
@@ -0,0 +1,3 @@
+pares = [("a", 3), ("b", 1), ("c", 2)]
+# Ordene com base no número.
+print(sorted(pares, key=lambda x: x[1]))
