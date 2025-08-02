1897d8a95546211ec48fed5edbddf8007f458601
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-02 11:53:02 -0300

    Atualização

--- Aprendizado/Curso/Funções Integradas/generator.py
+++ Aprendizado/Curso/Funções Integradas/generator.py
@@ -1,5 +1,7 @@
 """
+Generators são objetos iteráveis que geram valores sob demanda, usando a palavra-chave yield ou uma expressão de generator (como (x for x in iterable)).
 
+Eles não armazenam todos os valores na memória, mas sim produzem um por vez, à medida que são solicitados (preguiçosamente).
 """
 from sys import getsizeof
 
@@ -15,3 +17,6 @@ gen = contador()
 print(next(gen))  # 0
 print(next(gen))  # 1
 print(next(gen))  # 2
+
+nomes = ['Carol', 'Carolina', 'Angelica']
+print(any('C' in nome for nome in nomes))
