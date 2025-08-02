e6e9c4f6820614e6ce1c651c3feb5785c03a6475
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-02 14:30:38 -0300

    SAtualização

--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/dict.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/dict.py
@@ -1,3 +1,3 @@
 dados = {"ana": 9, "bruno": 7, "carlos": 10}
 # Transforme em lista e ordene os itens pelos valores.
-print(sorted(dados.values()))
+print(sorted(dados, key=lambda d: d[1]))
--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/idade.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sorted/idade.py
@@ -4,3 +4,4 @@ pessoas = [
     {"nome": "Carlos", "idade": 25}
 ]
 # Ordene a lista de pessoas pela idade.
+print(sorted(pessoas, key= lambda p: p['idade']))
