b0189336538d3ce51721ec8e51e362d5899a29d8
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-02 15:25:47 -0300

    Atualização

--- Aprendizado/Curso/Funções Integradas/reversed.py
+++ Aprendizado/Curso/Funções Integradas/reversed.py
@@ -4,3 +4,19 @@ reverse -> So em lista
 reversed -> Em tudo 
 """
 
+for letra in reversed("Arthur Resende"):
+    print(letra, end = "")
+
+print("\n")
+print("Arthur Resende"[::-1])
+
+nome = 'Arthur Resende'
+inverte = "".join(reversed(nome))
+print(inverte)
+
+for i in reversed(range(0,11)):
+    print(i, end=" ")
+
+print("\n")
+for i in range(10,-1,-1):
+    print(i, end=" ")
