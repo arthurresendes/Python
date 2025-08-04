844064e4be5e885b04942e7cc53f135a5445c93d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-04 14:47:29 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/abs/lista.py
@@ -0,0 +1,5 @@
+numerosAbs = [-5, -10, 3, -2]
+
+# Use abs() para transformar todos os elementos em seus valores absolutos → [5, 10, 3, 2]
+absoluto = list(abs(num) for num in numerosAbs)
+print(absoluto)
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/abs/sub02.py
@@ -0,0 +1,8 @@
+import random as rd
+
+x = rd.randint(1, 101)
+y = rd.randint(1000,100000)
+# Use abs(x - y) para garantir que o resultado seja sempre positivo → 15
+
+print(x-y)
+print(abs(x-y))
