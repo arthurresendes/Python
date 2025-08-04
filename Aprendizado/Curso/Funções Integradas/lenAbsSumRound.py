e58f6225a42a8416506244af655996cc8f1a584c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-04 14:56:11 -0300

    Atualização

---
+++ Aprendizado/Curso/Funções Integradas/lenAbsSumRound.py
@@ -0,0 +1,28 @@
+"""
+
+Len -> Tamanho de uma string , de uma lista , etc
+sum -> Soma valores de litsa , etc
+abs -> Valor absoluto , se for - vira mais
+round -> Arredonda numeros
+
+"""
+
+print(len("Arthur Resende"))
+
+print(len([1,2,3,4,5])) 
+
+print(abs(-5))
+
+print(abs(30-40))
+
+print(round(3.1411,2))
+
+print(round(4.7))
+
+print(round(2.2))
+
+print(sum([1,2,3,4,5]))
+
+# 5 no final seria como um frete para a lista de produtos
+
+print(sum([1,2,3,4,5] , 5))
