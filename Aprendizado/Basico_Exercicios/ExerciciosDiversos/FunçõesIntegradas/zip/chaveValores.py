034256d49052f7237274c86dc1362b7f38f0c8e1
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-04 14:54:19 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/zip/chaveValores.py
@@ -0,0 +1,4 @@
+chaves = ['a', 'b', 'c']
+valores = [1, 2, 3]
+
+print(dict(zip(chaves,valores)))
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/zip/escola.py
@@ -0,0 +1,5 @@
+nomes = ['Ana', 'Bruno', 'Carlos']
+notas1 = [7.5, 8.0, 9.2]
+notas2 = [6.5, 7.0, 8.5]
+
+print(list(zip(nomes,notas1,notas2)))
