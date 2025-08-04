e782b9eca9835c86e4870919b1322c9f7377aac3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-04 14:48:14 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sum/lista.py
@@ -0,0 +1,5 @@
+numeros = [10, 20, 30, 40]
+# Use sum() para obter a soma total da lista → 100
+
+soma = sum(numeros)
+print(soma)
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/sum/quadrado.py
@@ -0,0 +1,5 @@
+numeros2 = [1, 2, 3, 4]
+
+# Use sum() com compreensão de lista para somar os quadrados: 1² + 2² + 3² + 4² = 30
+quadrado = sum(num ** 2 for num in numeros2)
+print(quadrado)
