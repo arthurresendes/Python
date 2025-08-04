8d72ebe98f6b986dbee9e6448c8c95939ca822ef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-04 14:49:13 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/zip/mediaAlunos.py
@@ -0,0 +1,3 @@
+alunos = ["Lucas", "Marina", "André", "Juliana"]
+medias = [7.8, 6.5, 8.9, 5.7]
+print(list(zip(alunos,medias)))
---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/FunçõesIntegradas/zip/mercado.py
@@ -0,0 +1,5 @@
+produtos = ["Arroz", "Feijão", "Macarrão", "Leite"]
+
+precos = [5.50, 7.30, 4.20, 6.00]
+
+print(list(zip(produtos,precos)))
