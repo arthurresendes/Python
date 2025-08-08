8f7ef8381d5cb4ff149faca3fafffc2596d79161
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-08 14:01:07 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/modulos/geraSenha.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def gera_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
+
+print(gera_senha(8))
