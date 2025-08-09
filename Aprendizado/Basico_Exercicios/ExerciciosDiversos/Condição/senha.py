d342fcfd7b2604c834a50febe76adb079378fa9b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-09 15:05:07 -0300

    Atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Condição/senha.py
@@ -0,0 +1,6 @@
+nome = input("Digite o nome 'financeiro': ")
+senha = int(input("Digite a senha 303010: "))
+if nome == 'financeiro' and senha == 303010:
+    print("Bem-vindo!!")
+else:
+    print("Acesso negado!!")
