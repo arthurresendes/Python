abc616d8a85c852809800aa64407a62388f50a0b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-12 21:19:00 -0300

    Atualização

--- Aprendizado/Curso/Arquivos/lendoRead.py
+++ Aprendizado/Curso/Arquivos/lendoRead.py
@@ -15,6 +15,21 @@ with open(arquivo, "r", encoding="utf-8") as f:
     conteudo = f.read()
     print(conteudo)
 
+# Readlines para muitas linhas
+contador = 0
 with open(arquivoLong, 'r' , encoding='UTF-8') as file:
     conteudo2 = file.readline()
     print(conteudo2)
+    print(len(conteudo2)) # Conta quantas letras
+    print(len(conteudo2.split())) # Conta quantas palavras
+    for linhas in file:
+        contador += 1
+    print(contador)
+
+# Metodo acima funciona assim no meu vscode , tendo que importar o os para localizar onde esta o arquivo txt, mas tem outro metodos que funcionam bem
+
+try:
+    arquivos = open("texto.txt")
+    print(arquivos)
+except (FileNotFoundError,FileExistsError):
+    print("Não encontrado")
