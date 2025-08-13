d3505f24d6d008550e688af292abe159faa1f07d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-13 16:01:21 -0300

    Atualização

--- Aprendizado/Curso/Arquivos/with.py
+++ Aprendizado/Curso/Arquivos/with.py
@@ -2,19 +2,13 @@
 
 O bloco with eh utilizado para criar um contexto de trabalho , onde os recursos utilizados são fechados apos o bloco with
 
- 
-
 with open(arquivo , modo , enconding) as apelido:
 
-     conteudo = apelido.oque ira fazer
+conteudo = apelido.oque ira fazer
 
 '''
 
- 
-
 import os
-
- 
 
 arquivo = r"C:\Users\arthu\OneDrive\Área de Trabalho\Programação\Projetos\Python\Aprendizado\Curso\Arquivos\arquivoedita.txt"
 
--- Aprendizado/Curso/Arquivos/write.py
+++ Aprendizado/Curso/Arquivos/write.py
@@ -2,54 +2,33 @@
 
 Escrevendo em arquivos ("w")
 
- 
-
 Caso você escreva em um arqiuivo que ja tem um conteudo , ele ira sobrescrever e caso colocar o nome do arquivo e ele não existir ele ira ser criado
-
- 
 
 '''
 
- 
-
 import os
 
- 
-
-arquivo = r"C:\Users\arregomes\OneDrive - rd.com.br\Documentos\Python\Arquivos\escrita.txt"
-
- 
+arquivo = r"C:\Users\arthu\OneDrive\Área de Trabalho\Programação\Projetos\Python\Aprendizado\Curso\Arquivos\arquivoedita.txt"
 
 with open(arquivo , "w", encoding='UTF-8') as file:
-
     file.write("Escrever dados em arquivos eh facil\n")
-
     file.write("Podemos colocar quantas linhas quisermos\n")
-
     file.write("Ultima linha")
 
- 
 
 # Se colocar um arquivo que não existe ele eh criado
 
 with open('ola.txt' , "w", encoding='UTF-8') as file:
-
     file.write("Criando arquivo\n")
-
     file.write("Sem existir\n")
-
     file.write("Antes disso")
 
 
 
 
 with open(arquivo , 'r', encoding='UTF-8') as file:
-
     print(file.read())
 
- 
-
 with open('ola.txt' , 'r', encoding='UTF-8') as file:
-
     print(file.read())
 
