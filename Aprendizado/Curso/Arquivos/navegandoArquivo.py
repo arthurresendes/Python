2251c01dc8dbc17d2e1bbaabcdee631ec280229a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-18 12:02:56 -0300

    atualização

--- Aprendizado/Curso/Arquivos/navegandoArquivo.py
+++ Aprendizado/Curso/Arquivos/navegandoArquivo.py
@@ -4,6 +4,7 @@ os - Sistema Operacional
 '''
 
 import os
+import sys
 
 print(os.getcwd()) # Retorna onde o arquivo esta alocado
 
@@ -27,3 +28,17 @@ print(os.getcwd())
 
 print(os.path.isabs('/Programacao/Python'))
 print(os.path.isabs('C:\\Users\arthu'))
+
+# Nome do so
+print(os.name)
+
+# Outro modo de ver
+print(sys.platform)
+
+# Listando diretorios
+print(os.listdir('C://'))
+arquivos = list(os.scandir())
+scanner = os.scandir()
+print("\n")
+print(arquivos)
+scanner.close()
