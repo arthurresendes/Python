1f47a7f2f0e0e50b8e2382445e1279ed734a36f7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-19 22:27:02 -0300

    atualização

--- Aprendizado/Curso/Arquivos/manipulacao.py
+++ Aprendizado/Curso/Arquivos/manipulacao.py
@@ -2,3 +2,12 @@
 
 
 '''
+
+import os
+
+# Descobrindo se um arquivo ou diretorio existe
+
+print(os.path.exists('arquivos.txt'))
+print(os.path.exists('Aprendizado/Curso/Arquivo'))
+
+open('teste.txt', 'w').close()
