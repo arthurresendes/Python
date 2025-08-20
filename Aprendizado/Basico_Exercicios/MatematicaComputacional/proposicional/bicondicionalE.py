eefb026ec9289b8b7e5143f6f6f891d5f55a713d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-20 16:52:28 -0300

    atualização

---
+++ Aprendizado/Basico_Exercicios/MatematicaComputacional/proposicional/bicondicionalE.py
@@ -0,0 +1,20 @@
+p = True  
+q = False
+r = True
+result1 = None
+result2 = None
+
+if p and q == True:
+    result1 = True
+else:
+    result1 = False
+
+if not p == True and r == False:
+    result2 = False
+else:
+    result2 = True
+
+if result1 == result2:
+    print(True)
+else:
+    print(False)
