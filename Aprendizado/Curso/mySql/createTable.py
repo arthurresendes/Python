1024de686e5c9420d27950dae85a9487b9188a5c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-21 15:45:38 -0300

    atualização

--- Aprendizado/Curso/mySql/createTable.py
+++ Aprendizado/Curso/mySql/createTable.py
@@ -5,12 +5,12 @@ conexao = mysql.connector.connect(
     host = "",
     user = "",
     password = "",
-    database = "firma"
+    database = "tbcliente"
 )
 
 meu_cursor = conexao.cursor()
 
-meu_cursor.execute("CREATE DATABASE firma")
+meu_cursor.execute("CREATE table tbcliente (ID int auto_increment PRIMARY KEY ,nome VARCHAR(200), cpf UNIQUE VARCHAR(15))")
 
 for x in meu_cursor:
     print(x)
