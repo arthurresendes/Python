6b6555e2245aa6d5f638e7d231277c8e64d2e80b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-21 16:06:30 -0300

    atualização

--- Aprendizado/Curso/mySql/select.py
+++ Aprendizado/Curso/mySql/select.py
@@ -11,7 +11,7 @@ meu_cursor = conexao.cursor()
 
 meu_cursor.execute("SELECT * FROM tbclientes")
 
-meu_result = meu_cursor.fetchall() # - > Rever
+meu_result = meu_cursor.fetchall() # retorna uma lista de tuplas. Cada tupla contém os dados de uma linha da tabela. Se não houver mais linhas para retornar (por exemplo, a consulta retorna uma tabela vazia), fetchall() retornará uma lista vazia. Ou seja , faz retornar as linhas de uma consulta
 
 for x in meu_result:
     print(x)
