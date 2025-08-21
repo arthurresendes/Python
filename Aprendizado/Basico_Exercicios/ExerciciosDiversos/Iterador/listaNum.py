c2bd8156032368aaa4a5809a2695f7ae386e3aa2
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-21 15:10:29 -0300

    atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Iterador/listaNum.py
@@ -0,0 +1,10 @@
+numeros = [10, 20, 30, 40, 50]
+
+
+lista_ite = iter(numeros)
+
+print(next(lista_ite))
+print(next(lista_ite))
+print(next(lista_ite))
+print(next(lista_ite))
+print(next(lista_ite))
