fa6d45e2a09da72af3f042dc2ee7ddc1a1513700
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-26 19:10:22 -0300

    atualização

--- Aprendizado/Curso/Decoradores/assinaturas_diferentes.py
+++ Aprendizado/Curso/Decoradores/assinaturas_diferentes.py
@@ -9,8 +9,31 @@ def saudacao(nome):
     return f"Olá eu sou o {nome}"
 
 @gritar
-def ordenar(principal , acompanhamento = 'batata frita'):
+def ordenar(principal = 'carne parmegiana' , acompanhamento = 'batata frita'):
     return f"Gostaria de {principal} + {acompanhamento}"
 
 print(saudacao('Arthur'))
-print(ordenar("Carne parmegiana"))
+print(ordenar())
+
+
+def verifica_argumento(valor):
+    def funcao(func):
+        def wrapper(*args,**kwargs):
+            if args and args[0] != valor:
+                return f"Valor incorreto precisa ser {valor}"
+            return func(*args,**kwargs)
+        return wrapper
+    return funcao
+
+@verifica_argumento('pizza')
+def comida_favorita(*args):
+    return args
+
+@verifica_argumento(10)
+def soma_dez(num1,num2):
+    return num1 + num2
+
+print(soma_dez(10,2))
+print(soma_dez(20,2))
+print(comida_favorita('pizza', 'MC'))
+print(comida_favorita('KFC', 'Parmegiana'))
