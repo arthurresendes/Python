ea2f838d07e4785d016c94f6ecc4f67f06e4ae30
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-28 17:08:33 -0300

    atualização

---
+++ Aprendizado/Curso/POO/introducao.py
@@ -0,0 +1,30 @@
+'''
+
+- POO é um paradigma de programação que utiliza objetos e classes
+
+Classes - Modelo de objeto do mundo real representado computacionalmente
+
+Atributo - Caracteristoicas do objeto
+
+Metodo - Comportamento do objeto
+
+Construtor - Metodo especial utilizado para criar objetos
+
+Objeto - Instancia da classe
+
+'''
+
+numero = 10
+print(numero)
+print(type(numero))
+
+nome = 'Arthur'
+print(nome)
+print(type(nome))
+
+class Produto:
+    pass
+
+ps4 = Produto()
+print(ps4)
+print(type(ps4))
