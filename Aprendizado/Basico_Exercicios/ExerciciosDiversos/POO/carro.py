93535560b4a498a907d6bd901a85946a5d8f7ea9
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-29 17:42:10 -0300

    atualização

--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/POO/carro.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/POO/carro.py
@@ -1,12 +1,20 @@
 class Carro:
+    cont = 0
     def __init__(self,marca,modelo,ano):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
+        Carro.cont += 1
 
     def exibir_detalhes(self):
         print(f"Marca: {self.marca}\nModelo: {self.modelo}\nAno: {self.ano}")
+    
+    @classmethod
+    def mostraQuantos(cls):
+        return cls.cont
 
 car1 = Carro('Fusca', 'Volkswagen', 1980)
 
 car1.exibir_detalhes()
+
+print(Carro.mostraQuantos())
