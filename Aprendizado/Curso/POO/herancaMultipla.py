a85ea569c1b900400a13b6b300ea29af82c038ba
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-01 17:33:05 -0300

    atualização

---
+++ Aprendizado/Curso/POO/herancaMultipla.py
@@ -0,0 +1,47 @@
+class Animal:
+    def __init__(self, nome):
+        self._nome = nome
+
+    def cumprimentar(self):
+        return f"Ola sou {self._nome}"
+
+class Aquatico(Animal):
+    def __init__(self,nome):
+        super().__init__(nome)
+
+    def nadar(self):
+        return f"Sei nadar"
+
+    def cumprimentar(self):
+        return f"Ola sou {self._nome} do mar"
+
+class Terrestre(Animal):
+    def __init__(self,nome):
+        super().__init__(nome)
+
+    def andar(self):
+        return f"Sei andar"
+
+    def cumprimentar(self):
+        return f"Ola sou {self._nome} da terra"
+
+class Pinguim(Aquatico, Terrestre):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def seiTudo(self):
+        return f"Se andar e nadar"
+
+    def cumprimentar(self):
+        return f"Ola sou {self._nome} da terra"
+
+tatu = Terrestre('Tat')
+baleia = Aquatico('Bale')
+pinguim = Pinguim('Pingui')
+
+print(tatu.cumprimentar())
+print(tatu.andar())
+print(baleia.cumprimentar())
+print(baleia.nadar())
+print(pinguim.cumprimentar())
+print(pinguim.seiTudo())
