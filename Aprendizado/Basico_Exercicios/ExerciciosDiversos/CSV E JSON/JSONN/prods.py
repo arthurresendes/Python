c45d41430c1a0a7a670f538c50c2503eaa496990
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-06 13:59:02 -0300

    atualização

--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/CSV E JSON/JSONN/prods.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/CSV E JSON/JSONN/prods.py
@@ -2,8 +2,19 @@ import json
 
 produtos = {
     "clientes": [
-        {"produto": "Cafe", "preco": 8.5},
-        {"produto": "Arroz", "preco": 8.5},
-        {"produto": "Vingadores", "preco": 8.5},
+        {"produto": "Cafe", "preco": 22.10, "quantidade": 3},
+        {"produto": "Arroz", "preco": 11.5,"quantidade": 2},
+        {"produto": "Feijão", "preco": 12, "quantidade": 1},
     ]
 }
+
+with open("prods.json", "w") as file:
+    json.dump(produtos,file,indent=4)
+
+soma = 0
+
+with open("prods.json", "r") as file:
+    dados = json.load(file)
+    for item in dados["clientes"]:
+        soma = soma +  (item["preco"] * item["quantidade"])
+        print(f"{soma} reais do produto {item["produto"]}")
