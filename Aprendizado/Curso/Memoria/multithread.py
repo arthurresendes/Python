2ed08b3d1c26f2f7f88c99333dfd413ae1a84a41
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-06 13:27:50 -0300

    atualização

--- Aprendizado/Curso/Memoria/multithread.py
+++ Aprendizado/Curso/Memoria/multithread.py
@@ -7,8 +7,14 @@ def contagem_regresiva(x):
     while x > 0:
         x = x - 1
 
+t1 = Thread(target=contagem_regresiva, args=(CONTADOR//2,))
+t2 = Thread(target=contagem_regresiva, args=(CONTADOR//2,))
+
 inicio = time.time()
-contagem_regresiva(CONTADOR)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 fim = time.time()
 
 print(f"Demorou {fim - inicio} segundos")
