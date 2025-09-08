ff6f97686230554dcda03aac900ab1b7f58153ba
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-08 16:44:28 -0300

    atualização

---
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Tipagem/concatena.py
@@ -0,0 +1,4 @@
+def concatena(palavra1: str, palavra2: str) -> str:
+    return f"{palavra1 + " " + palavra2}"
+
+print(concatena("Arthur", "Resende"))
--- Aprendizado/Basico_Exercicios/ExerciciosDiversos/Tipagem/inverte.py
+++ Aprendizado/Basico_Exercicios/ExerciciosDiversos/Tipagem/inverte.py
@@ -1,2 +1,4 @@
 def inverte_string(palavra: str) -> str:
     return palavra[::-1]
+
+print(inverte_string("Arthur"))
