70b6332ef6c7b0724812fa134d79ecf3036d892a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-08 16:39:26 -0300

    atualização

---
+++ Aprendizado/Curso/Tipagem/dinamicos.py
@@ -0,0 +1,19 @@
+class CisneNegro:
+    def __len__(self):
+        return 42
+
+livro = CisneNegro()
+print(len(livro))
+
+nome = "Arthur Resende"
+nums = [12,34,45,49]
+dicio = {"carlos": 12, "joao": 22, "arthur": 18}
+print(len(nome))
+print(len(nums))
+print(len(dicio))
+#                          ^
+# Similares , mesmo metodo |
+
+
+idade = 42
+peso = 81.4
