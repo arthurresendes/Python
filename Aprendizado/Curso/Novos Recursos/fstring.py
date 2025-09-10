bca575716b7f508927e9332bfd3d0687639fe4a3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-10 16:33:36 -0300

    atualização

---
+++ Aprendizado/Curso/Novos Recursos/fstring.py
@@ -0,0 +1,12 @@
+
+def multiplica(num1 : float , num2: float) -> float:
+    return num1 + num2
+
+resultado = multiplica(6.23,7.343)
+print(f"O resultado é {resultado:.2f}")
+
+print(f"{(media:=8/2)} é a metade de {media * 2}")
+
+nome : str = "Arthur"
+print(f"{nome=}")
+print(f"{nome.upper()[::-1]=}")
