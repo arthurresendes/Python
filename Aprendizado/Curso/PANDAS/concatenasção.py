3ada7277e1458c1e224ed374326b63204de87705
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-13 12:26:00 -0300

    atualização

--- Aprendizado/Curso/PANDAS/concatenasção.py
+++ Aprendizado/Curso/PANDAS/concatenasção.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 dados = [
     {'Nome': 'Ana', 'Idade': 25, 'Cidade': 'São Paulo'},
     {'Nome': 'Bruno', 'Idade': 30, 'Cidade': 'Rio de Janeiro'},
@@ -11,12 +10,6 @@ dados2 = {'Nome': ['Ana', 'Bruno','Carla'],
         'Cidade': ['São Paulo', 'Belo Horizonte', 'Rio de Janeiro']
         }
 df = pd.DataFrame(dados)
-print(df[['Nome','Idade']])
-print("\n")
 df2 = pd.DataFrame(dados2)
-adulto = df2[df2['Idade'] > 27]
-print(adulto)
-print(df.columns)
-print(df.to_numpy())
-
-df.to_excel("InformaçõesAlunos.xlsx", index=True)
+concatena = pd.concat([df,df2])
+print(concatena)
---
+++ Aprendizado/Curso/PANDAS/visualizandoDados.py
@@ -0,0 +1,9 @@
+'''
+df.head() -> Ver o começo da tabela 
+df.tail() -> Ver a o final da tabela
+df.index
+df.columns
+df.to_numpy() -> Retorna os valores da tabela
+df.T -> transforma linha em coluna e coluna em linha
+
+'''
