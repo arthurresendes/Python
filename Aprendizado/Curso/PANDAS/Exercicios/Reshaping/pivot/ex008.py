f3631d4db90787ce06e77ed398c80145bfd7db02
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-16 18:57:23 -0300

    atualização

--- Aprendizado/Curso/PANDAS/Exercicios/Reshaping/pivot/ex008.py
+++ Aprendizado/Curso/PANDAS/Exercicios/Reshaping/pivot/ex008.py
@@ -2,3 +2,16 @@
 ['Cidade', 'Produto', 'Vendas']
 '''
 
+import pandas as pd
+
+def main():
+    df = pd.DataFrame({
+    'Produto': ['PC','Cafe','Livros','Cafe'],
+    'Cidade': ['Osasco','SP','Belo horizonte','SP'],
+    'Vendas': [200,300,552,236]
+    })
+    pivot = df.pivot_table(index='Cidade', columns='Produto' , values='Vendas', aggfunc="max")
+    print(pivot)
+
+if __name__ == "__main__":
+    main()
