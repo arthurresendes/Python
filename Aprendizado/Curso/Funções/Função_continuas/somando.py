f87a8bb0da260099945637814c46b718f15f0ea3
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-29 16:39:22 -0300

    atualização

---
+++ Aprendizado/Curso/Funções/Função_continuas/somando.py
@@ -0,0 +1,9 @@
+def recebe_numero(num):
+    return num * 2
+
+def somando_depois_de_dobrar(number):
+    return number + 10
+
+numero = recebe_numero(10)
+somando = somando_depois_de_dobrar(numero)
+print(somando)
