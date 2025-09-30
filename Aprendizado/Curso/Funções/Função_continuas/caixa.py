c545024d71deb267e5ac423cafcb7b24bd8405ca
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-30 17:47:28 -0300

    atualização

---
+++ Aprendizado/Curso/Funções/Função_continuas/caixa.py
@@ -0,0 +1,35 @@
+import sys
+def saque_multiplo_dez(valor: int):
+    if valor % 10 == 0:
+        return "É multiplo"
+    else:
+        sys.exit()
+
+# // é divisão inteira
+
+def notas_saque(valor):
+    saque_multiplo_dez(valor)
+    nota50 = valor // 50
+    resto = valor % 50
+        
+    nota20 = resto // 20
+    resto = resto % 20
+        
+    nota10 = resto // 10
+        
+    return nota50,nota20,nota10
+
+def valor_saque():
+    while True:
+        try:
+            saque = int(input("Qual valor a ser sacado: "))
+            if isinstance(saque,int):
+                break
+        except:
+            print("Digite um número inteiro!!")
+    n50,n20,n10 = notas_saque(saque)
+    print(f"{n50,n20,n10}")
+
+
+if __name__ == "__main__":
+    valor_saque()
