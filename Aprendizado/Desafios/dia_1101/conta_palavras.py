4719733458777055a2d4f0ce2752a72b2f598981
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-11 11:34:27 -0300

    Contagem palavras e palavra mais utilizada

---
+++ Aprendizado/Desafios/dia_1101/conta_palavras.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def contagem(frase: str) -> list[int,str]:
+    palavras = frase.split()
+    contador_palavras = len(palavras)
+    
+    palavra_mais_utilizada = max(set(palavras), key=palavras.count)
+    return [contador_palavras,palavra_mais_utilizada]
+
+
+print(contagem("Ola mundo O mundo é divertido"))
