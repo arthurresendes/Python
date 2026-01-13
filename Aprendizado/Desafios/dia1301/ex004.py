abd46a6ccc3e535406ca6ef0fee90ce371e79dbf
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-13 11:04:52 -0300

    Exercicio 04

--- Aprendizado/Desafios/dia1301/ex004.py
+++ Aprendizado/Desafios/dia1301/ex004.py
@@ -1,10 +1,14 @@
-populacaoA = 8000
-populacaoB = 200000
+def calculo_pop() -> str:
+    populacaoA = 8000
+    populacaoB = 200000
 
-anos = 0
-while populacaoA < populacaoB:
-    populacaoA *= 1.03
-    populacaoB *= 1.02
-    anos += 1
+    anos = 0
+    while populacaoA < populacaoB:
+        populacaoA *= 1.03
+        populacaoB *= 1.02
+        anos += 1
 
-print(f"Demorou {anos} anos para Populacão A: {populacaoA} passar a População B: {populacaoB}")
+    return f"Demorou {anos} anos para Populacão A: {populacaoA} passar a População B: {populacaoB}"
+
+if __name__ == "__main__":
+    print(calculo_pop())
